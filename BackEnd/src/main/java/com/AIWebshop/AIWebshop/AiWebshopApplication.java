48edbc197d8c30106768982470da8a66121e2c51
Erdélyi Patrik <erdelyi.patrik99@gmail.com>
2024-09-18 15:41:14 +0200

    Backend változtatások

--- BackEnd/src/main/java/com/AIWebshop/AIWebshop/AiWebshopApplication.java
+++ BackEnd/src/main/java/com/AIWebshop/AIWebshop/AiWebshopApplication.java
@@ -1,4 +1,4 @@
-package com.webshop.AIWebshop;
+package com.AIWebshop.AIWebshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- BackEnd/src/test/java/com/AIWebshop/AIWebshop/AiWebshopApplicationTests.java
+++ BackEnd/src/test/java/com/AIWebshop/AIWebshop/AiWebshopApplicationTests.java
@@ -1,4 +1,4 @@
-package com.webshop.AIWebshop;
+package com.AIWebshop.AIWebshop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
