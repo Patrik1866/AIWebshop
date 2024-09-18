7104c40ba64860cac698c28381d43f8eef564339
Erdélyi Patrik <erdelyi.patrik99@gmail.com>
2024-09-18 13:42:36 +0200

    BackEnd server generálása

---
+++ BackEnd/src/main/java/com/webshop/AIWebshop/AiWebshopApplication.java
@@ -0,0 +1,13 @@
+package com.webshop.AIWebshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiWebshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiWebshopApplication.class, args);
+	}
+
+}
---
+++ BackEnd/src/test/java/com/webshop/AIWebshop/AiWebshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webshop.AIWebshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiWebshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
