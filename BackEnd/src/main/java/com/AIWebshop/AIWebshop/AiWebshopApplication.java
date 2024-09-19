f2d8741bd967ad54f53f7fdb4fed45f4bf88c2aa
Erdélyi Patrik <erdelyi.patrik99@gmail.com>
2024-09-19 14:33:09 +0200

    Cors config and basic controller

--- BackEnd/src/main/java/com/AIWebshop/AIWebshop/AiWebshopApplication.java
+++ BackEnd/src/main/java/com/AIWebshop/AIWebshop/AiWebshopApplication.java
@@ -2,6 +2,9 @@ package com.AIWebshop.AIWebshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class AiWebshopApplication {
@@ -10,4 +13,14 @@ public class AiWebshopApplication {
 		SpringApplication.run(AiWebshopApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfig(){
+		return new WebMvcConfigurer() {
+			public void addCorsMapping(CorsRegistry reg){
+				reg.addMapping("/api/**")
+						.allowedOrigins("http://localhost:3000")
+						.allowedMethods("GET", "POST", "PUT", "DELETE");
+			}
+		};
+	}
 }
