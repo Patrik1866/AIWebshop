4018f2a97f0b75d0999d1143c8a91785b4f4d2cf
Erdélyi Patrik <erdelyi.patrik99@gmail.com>
2024-10-02 19:47:42 +0200

    [backend] login/register API

--- BackEnd/src/main/java/com/AIWebshop/AIWebshop/Fields/User.java
+++ BackEnd/src/main/java/com/AIWebshop/AIWebshop/Fields/User.java
@@ -2,49 +2,63 @@ package com.AIWebshop.AIWebshop.Fields;
 
 import jakarta.persistence.*;
 
-
 @Entity
 @Table(name= "users")
 public class User {
-    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-
-    private Integer id;
-    private String first_name;
-    private String surename;
-    private String email;
-    private String password;
 
     public Integer getId() {
         return id;
     }
+
     public void setId(Integer id) {
         this.id = id;
     }
-    public String getFirst_name() {
-        return first_name;
+
+    public String getSurname() {
+        return surname;
     }
-    public void setFirst_name(String first_name) {
-        this.first_name = first_name;
+    public void setSurname(String surname) {
+        this.surname = surname;
     }
+
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
     public String getPassword() {
         return password;
     }
+
     public void setPassword(String password) {
         this.password = password;
     }
-    public String getSurename() {
-        return surename;
+
+    public String getPhone() {
+        return phone;
     }
 
-    public void setSurename(String surename) {
-        this.surename = surename;
+    public void setPhone(String phone) {
+        this.phone = phone;
     }
-
+    @Id
+    @Column(name ="user_id")
+    private Integer id;
+    private String surname;
+    private String firstname;
+    private String email;
+    private String password;
+    private String phone;
 }
