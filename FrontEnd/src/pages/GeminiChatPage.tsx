b11bf648a497da58f50c66376f8793873e3ffe79
Erdélyi Patrik <erdelyi.patrik99@gmail.com>
2025-01-16 22:49:12 +0100

    [Backend/Frontend] Chat kérdések válaszok

--- FrontEnd/src/pages/GeminiChatPage.tsx
+++ FrontEnd/src/pages/GeminiChatPage.tsx
@@ -1,11 +1,13 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "../styles/gemini.css"
 import { useAuth } from "../util/AuthContext";
+
+
 
 const GeminiChatPage = () => {
     const {user} = useAuth();
     const [message, setMessage] = useState("");
-    const [response, setResponse] = useState("");
+    const [responses, setResponse] = useState<{question: string, message: string}[]>([]);
 
     const handleSendMessage = async () => {
         try{
@@ -19,7 +21,33 @@ const GeminiChatPage = () => {
                 body: JSON.stringify(chatRequest)
             });
 
-            const responseData = await response.text();
+            const responseData = await response.json();
+           
+            const messages = responseData.map((message: any)=>({
+                question: message.question,
+                message: message.message
+            }))
+            setResponse(messages);
+        } catch (error){
+            console.error(error);
+        }
+    };
+
+    useEffect(() => {
+        handleGetMessages();
+    }, []);
+
+    const handleGetMessages = async () => {
+        try{
+            const response = await fetch(`http://localhost:8080/chat/geminiMessage/${user?.id}`, {
+                method: "GET",
+                headers: {
+                    "Authorization": `Bearer ${sessionStorage.getItem("token")}`,
+                    "Content-Type": "application/json"
+                }
+            });
+            const responseData = await response.json();
+
             setResponse(responseData);
         } catch (error){
             console.error(error);
@@ -40,7 +68,12 @@ const GeminiChatPage = () => {
         <div className="chat-container">
             <div className="chat-messages">
                 <p>Üzenetek megjelennek itt</p>
-                {response && <p>Válasz: {response}</p>}
+                {responses.map((response, index) => (
+                    <div key={`${index}_${response.question}_${response.message}`}>
+                        <span>Kérdés: {response.question}</span>
+                        <span>Válasz: {response.message}</span>
+                    </div>
+                ))}
             </div>
             <div className="chat-input">
                 <input type="text" id="chat-input" placeholder="Üzenet" value={message} onChange={handleInputChange}/><br />
