c6b4e5d66d6d473991dcaac5c20d7d794d146cce
Erdélyi Patrik <erdelyi.patrik99@gmail.com>
2025-03-17 22:39:23 +0100

    [teszt]

---
+++ BackEnd/src/main/java/com/AIWebshop/AIWebshop/Config/EmailConfig.java
@@ -0,0 +1,31 @@
+package com.AIWebshop.AIWebshop.Config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class EmailConfig {
+
+    Dotenv dotenv = Dotenv.load();
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+        mailSender.setUsername(dotenv.get("EMAIL_USERNAME"));
+        mailSender.setPassword(dotenv.get("EMAIL_KEY"));
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+
+        return mailSender;
+    }
+}
